c2f3d82435e68e55b118634f96aafaf5e05bcab7
MKA <8120158+mkahvi@users.noreply.github.com>
2019-04-23 18:59:49 +0300

    Bring time tests over

---
+++ UnitTests/TimeTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MKAh;
+using NUnit;
+using NUnit.Framework;
+
+namespace Timing
+{
+	[TestFixture]
+	public class Time
+	{
+		[Test]
+		[TestOf(nameof(MKAh.Native.GetTickCount))]
+		[Retry(3)]
+		public void GetTickCountEquality()
+		{
+			uint eticks = (uint)Environment.TickCount;
+			uint pticks = MKAh.Native.GetTickCount();
+
+			Console.WriteLine("C# Env:   " + eticks);
+			Console.WriteLine("P/Invoke: " + pticks);
+
+			Assert.AreEqual(eticks/1000, pticks/1000);
+		}
+
+		[Test]
+		[TestOf(nameof(MKAh.User.CorrectIdleTime))]
+		[Retry(3)]
+		public void GetTickCountCorrection()
+		{
+			Assert.AreEqual(10_000L, MKAh.User.CorrectIdleTime(12_000, 22_000), "Normal");
+		}
+
+		[Test]
+		[TestOf(nameof(MKAh.User.CorrectIdleTime))]
+		[Retry(3)]
+		public void GetTickCountWrapAround()
+		{
+			uint diff = 5_000;
+			uint lowTick = diff;
+			uint highTick = uint.MaxValue - diff;
+
+			long result = MKAh.User.CorrectIdleTime(highTick, lowTick);
+
+			Console.WriteLine($"GetTickCount: {highTick} -> {lowTick} = {result}");
+
+			Assert.AreEqual(diff*2L, result, "49 day wrap around failpoint");
+		}
+	}
+}
