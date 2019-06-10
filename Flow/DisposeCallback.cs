f050ca8f17b756e6f72784f6697096e560c4c5d6
MKA <8120158+mkahvi@users.noreply.github.com>
2019-06-10 19:02:23 +0300

    Added OnDispose.Callback(Action)

---
+++ Flow/DisposeCallback.cs
@@ -0,0 +1,72 @@
+﻿//
+// MKAh.Flow.DisposeCallback.cs
+//
+// Author:
+//       M.A. (https://github.com/mkahvi)
+//
+// Copyright (c) 2019 M.A.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+
+namespace MKAh.Flow
+{
+	public class OnDispose : IDisposable
+	{
+		internal Action disposeAction = null;
+
+		internal OnDispose(Action action) {
+			disposeAction = action;
+		}
+
+		public static OnDispose Callback(Action action)
+		{
+			if (action is null) throw new ArgumentNullException(nameof(action));
+
+			return new OnDispose(action);
+		}
+
+		#region IDisposable Support
+		private bool disposed = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					disposeAction?.Invoke();
+					disposeAction = null;
+				}
+
+				disposed = true;
+			}
+		}
+
+		~OnDispose() => Dispose(false);
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+}
