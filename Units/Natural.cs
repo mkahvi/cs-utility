0a41c8de140e6e483870c5f9865e74074dbc78a3
MKA <8120158+mkahvi@users.noreply.github.com>
2019-08-15 16:36:29 +0300

    Binary units

---
+++ Units/Natural.cs
@@ -0,0 +1,40 @@
+﻿//
+// Units.Natural.cs
+//
+// Author:
+//       M.A. (https://github.com/mkahvi)
+//
+// Copyright (c) 2017-2018 M.A.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+namespace MKAh.Units
+{
+	public static class Binary
+	{
+		public const long Base = 2; // 1 << 1, 0b10
+
+		public const long Multiplier = 1024;
+
+		public const long Kilo = Multiplier; // short will go up to 32-64 kilos
+		public const long Mega = Kilo * Multiplier;
+		public const long Giga = Mega * Multiplier; // int will go up to 2-4 gigas
+		public const long Tera = Giga * Multiplier; // requires long
+	}
+}
