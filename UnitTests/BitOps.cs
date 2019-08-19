88da7c72722e012fb77099e1cf2612760d3846b4
MKA <8120158+mkahvi@users.noreply.github.com>
2019-08-19 09:32:43 +0300

    Bit ops testing

---
+++ UnitTests/BitOps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MKAh;
+using NUnit;
+using NUnit.Framework;
+
+namespace Logic
+{
+	[TestFixture]
+	public class Bit
+	{
+		[Test]
+		[TestOf(nameof(MKAh.Logic.Bit.Move))]
+		[TestCase(0b0100, 0b0010, ExpectedResult = 0b0010)]
+		public int Move(int source, int target)
+		{
+			int result = MKAh.Logic.Bit.Move(source, target);
+
+			Assert.AreEqual(target, result, "{0} does not match {1}", Convert.ToString(result, 2).PadLeft(4), Convert.ToString(target, 2).PadLeft(4));
+
+			return result;
+		}
+	}
+}
