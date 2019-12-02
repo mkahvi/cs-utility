7df4e96fe97207d3ae7c65ea37da8594cfd86658
MKA <8120158+mkahvi@users.noreply.github.com>
2019-12-02 17:11:42 +0200

    Cleanup usings

--- Data/JSON/JSON.cs
+++ Data/JSON/JSON.cs
@@ -24,13 +24,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MKAh.Data
 {
 	public partial class JSON
